b1ef7d9ae36e341ce5887a55184dde65331aafd2
madhushree-capg <MADHUSHREE.N@CAPGEMIN.COM>
2025-09-26 17:05:15 +0530

    OrderManagementSystem

--- OrderManagementSystemApplication/src/main/java/com/example/OrderManagementSystemApplication/OrderManagementSystemApplication.java
+++ OrderManagementSystemApplication/src/main/java/com/example/OrderManagementSystemApplication/OrderManagementSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrderManagementSystemApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagementSystemApplication.class, args);
 	}
